651414ab7d02c8d87e3169f9a2d6bc4c23c2f8ac
Tony Nguyen <nhattruong1811@gmail.com>
2023-04-16 09:06:32 +0700

    test merge

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <h1>Add too truong</h1>
       <h3>push lan 2</h3>
+      <h3>test merge</h3>
       {
         Object.keys(models).map( workspace_id => {
           return <LuminoLayout
